0957146557b99a9476839d57a95289a7b0ee5f00
Michał Krzyżanowski <Miisza95@gmail.com>
2018-09-22 19:22:50 +0200

    make zaj1zad1

---
+++ zaj1zad1.py
@@ -0,0 +1,4 @@
+firstDelicacy = input("Podaj pierwszy przysmak: ")
+secondDelicacy = input("Podaj drugi przysmak: ")
+merge = firstDelicacy + secondDelicacy
+print("Wynik łączenia nazw obu przysmaków: ", merge)
